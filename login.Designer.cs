07447a22458beb9162217691d7b22503e9671736
Harry Giddens <HG419@student.aru.ac.uk>
2023-10-01 14:15:40 +0100

    Add project files.

---
+++ login.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace testGUI
+{
+    partial class loginWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginWindow));
+            this.koiPictureBox = new System.Windows.Forms.PictureBox();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.koiPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // koiPictureBox
+            // 
+            this.koiPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("koiPictureBox.Image")));
+            this.koiPictureBox.Location = new System.Drawing.Point(185, 70);
+            this.koiPictureBox.Name = "koiPictureBox";
+            this.koiPictureBox.Size = new System.Drawing.Size(113, 105);
+            this.koiPictureBox.TabIndex = 0;
+            this.koiPictureBox.TabStop = false;
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLabel.ForeColor = System.Drawing.Color.White;
+            this.loginLabel.Location = new System.Drawing.Point(210, 197);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(60, 25);
+            this.loginLabel.TabIndex = 1;
+            this.loginLabel.Text = "Login";
+            this.loginLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(138, 357);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(213, 36);
+            this.confirmButton.TabIndex = 2;
+            this.confirmButton.Text = "Confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(138, 256);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(213, 22);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(138, 305);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(213, 22);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.ForeColor = System.Drawing.Color.White;
+            this.usernameLabel.Location = new System.Drawing.Point(35, 256);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(97, 22);
+            this.usernameLabel.TabIndex = 5;
+            this.usernameLabel.Text = "Username:";
+            this.usernameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.ForeColor = System.Drawing.Color.White;
+            this.passwordLabel.Location = new System.Drawing.Point(35, 305);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(94, 22);
+            this.passwordLabel.TabIndex = 6;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // loginWindow
+            // 
+            this.AcceptButton = this.confirmButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(490, 639);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.koiPictureBox);
+            this.Name = "loginWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.loginWindow_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.koiPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox koiPictureBox;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+    }
+}
+
---
+++ login.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace testGUI
+{
+    public partial class loginWindow : Form
+    {
+        public loginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            // Set up a string with the MySQL database login information
+            string testdbConnectionString = "Server=localhost;Database=testdb;User ID=root;Password=frog11";
+            MySqlConnection connection = new MySqlConnection(testdbConnectionString);
+
+            // Try connecting to the database if this fails print the error message in a box
+            try 
+            {
+                connection.Open();
+
+                string username = txtUserName.Text;
+                string password = txtPassword.Text;
+
+                // This SELECT statement searches the table users for a row with a matching username and password
+                // the BINARY command just makes sure its case sensitive
+                string loginQuery = "SELECT * FROM users WHERE BINARY username = @username AND BINARY password = @password";
+
+                // This object executes the MySQL commands
+                MySqlCommand cmd = new MySqlCommand(loginQuery, connection);
+
+                // @username and @password are just placeholders in the statement this puts in the actual values
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                /* The ExecuteScalar command returns the first row that matches the username and password
+                 * this means that if it does not find a match the object will be set to null which we can test
+                 * with a simple if statement
+                 */
+                object result = cmd.ExecuteScalar();
+
+                if (result != null)
+                {
+                    new dashboard().Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Fucking moron");
+                    txtUserName.Clear();
+                    txtPassword.Clear();
+                }
+
+                // Closing the connection to the database before moving on
+                connection.Close();
+            }
+
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+
+        }
+
+        private void loginWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                // Ensure that the form is being closed by the user (not programmatically).
+                Application.Exit();
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            txtPassword.PasswordChar = '•';
+        }
+    }
+}
